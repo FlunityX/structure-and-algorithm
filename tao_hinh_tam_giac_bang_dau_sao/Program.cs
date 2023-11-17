b43586ed6d64ba5f72a6265aa7f36b057e393dd2
FlunityX <149858650+FlunityX@users.noreply.github.com>
2023-11-17 16:12:43 +0700

    giải thuật

---
+++ tao_hinh_tam_giac_bang_dau_sao/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace tao_hinh_tam_giac_bang_dau_sao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("nhap so dong chua tam giac ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i= 1; i < n; i++)
+            {
+                string spaces = new string(' ', n - i);
+                string star = new string('*' ,2 * i- 1);
+                Console.WriteLine(spaces+star);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
