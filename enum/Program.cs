54f820b83722ace679eb7ea91514e793fe082d1d
FlunityX <149858650+FlunityX@users.noreply.github.com>
2023-12-05 23:41:59 +0700

    kiểu dữ liệu enum

---
+++ enum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EnumExample
+{
+    class Program
+    {
+        enum Thutrongtuan
+        {
+            ThuHai,
+            ThuBa,
+            ThuTu,
+            ThuNam,
+            ThuSau,
+            ThuBay,
+            ChuNhat
+        }
+
+        static void Main(string[] args)
+        {
+            Thutrongtuan thuHienTai = Thutrongtuan.ThuBa;
+            Console.WriteLine("hom nay la " + thuHienTai.ToString());
+            Thutrongtuan ngaymai = Thutrongtuan.ThuTu;
+            Console.WriteLine("ngay mai la " + ngaymai.ToString());
+        }
+    }
+}
