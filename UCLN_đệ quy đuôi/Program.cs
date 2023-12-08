862c3aca7bebe9bf1e999a788abf7e8335628d5c
FlunityX <149858650+FlunityX@users.noreply.github.com>
2023-12-08 23:54:25 +0700

    đệ quy

---
+++ UCLN_đệ quy đuôi/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace UCLN_de_quy_duoi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ucln = UCLN(8, 4);
+            Console.WriteLine("Uoc chung lon nhat la: " + ucln);
+            Console.ReadLine();
+        }
+
+        static int UCLN(int a, int b)
+        {
+            if (b == 0)
+            {
+                return a;
+            }
+            else
+            {
+                return UCLN(b, a % b);
+            }
+            // 8,4 thì b khác 0 => 4,0 => 4 
+        }
+    }
+}
---
+++ đệ_quy_đuôi/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace đệ_quy_đuôi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tongcuadayso = tongcuacacso(6, 0);
+            Console.WriteLine("tong cua day so la :" + tongcuadayso);
+            Console.ReadLine();
+        }
+
+        static int tongcuacacso(int n, int acc)
+        {
+            if (n <= 0)
+            {
+                return acc;
+            }
+            //Là một dạng đặc biệt của đệ quy.
+            //Khi một hàm gọi chính nó ở dạng cuối cùng của nó, tức là không có thêm bất kỳ xử lý nào sau khi gọi đệ quy.
+    // giúp trình biên dịch tối ưu hóa bằng cách loại bỏ các frame của hàm đệ quy khỏi ngăn xếp(stack), giảm bớt sự tiêu tốn bộ nhớ.
+            return n + tongcuacacso(n - 1, acc +n );
+            //  6 thì return 6+(5,6)
+            //  5 thì return 6+5+(4,11)
+            //  4 thì return 6+5+4+(3,15)
+            //...
+            // 0  xuất ra acc = 6+5+4+3+2+1+ tongcuacacso (0,21) =42
+
+        }
+    }
+}
