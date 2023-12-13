8e90904acdc13067582fce8b52ee3f0f8af2ca28
FlunityX <149858650+FlunityX@users.noreply.github.com>
2023-12-13 13:01:56 +0700

    áp dụng thuật toán đệ quy

---
+++ tính_tổng_dãy_số/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace tính_tổng_dãy_số
+{
+    internal class Program
+    {
+        // cho day so t(n) = n^2 + (n-1)^2  
+        // S(n) = t(1) + t(2) + t(n) + ...
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhập số n: ");
+            int n = int.Parse(Console.ReadLine());
+            double sum = tinhtong(n);
+            Console.WriteLine("Tổng của dãy số là: {0}", sum);
+            Console.ReadLine();
+        }
+
+        static double tinhtong(int n)
+        {
+            double sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                double t = Math.Pow(i, 2) - Math.Pow(i - 1, 2);
+                sum += t;
+            }
+            return sum;
+        }
+    }
+}
