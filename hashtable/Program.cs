5ce7b3b79535b09f82604677ccf0d5833d9688ce
FlunityX <149858650+FlunityX@users.noreply.github.com>
2024-01-21 01:09:34 +0700

    hashtable cơ bản

---
+++ hashtable/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+namespace hashtable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable hash = new Hashtable();
+            hash.Add(1, "e");
+            hash.Add(2, "d");
+            hash.Add(3, "g");
+            Console.WriteLine("so phan t cua hash la "+hash.Count);
+            foreach(DictionaryEntry a in  hash)
+            {
+                Console.WriteLine("key {0} , value {1}" ,a.Key,a.Value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
