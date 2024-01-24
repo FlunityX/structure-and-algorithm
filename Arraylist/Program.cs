a04604ae87b91b76b94f87c57a92c7e6ae4e18e3
FlunityX <149858650+FlunityX@users.noreply.github.com>
2024-01-24 10:07:13 +0700

    mảng động trong c#

---
+++ Arraylist/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+namespace Arraylist
+{
+    internal class Program
+    {
+        // là 1 mảng động trong c#
+        static void Main(string[] args)
+        {
+          ArrayList my = new ArrayList();
+            my.Add(1);
+            my.Add(2);
+            my.Add(3);
+            my.Add("hello");
+            my[3] = 4;
+            foreach (int i in my)
+            {
+                Console.WriteLine("key :{0} " ,i);
+            }
+         Console.WriteLine("so luong phan tu la :{0}" ,my.Count);
+           Console.ReadLine();
+        }
+    }
+}
