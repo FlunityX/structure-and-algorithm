127f3d7a5fcc619bacfb14617573b300b5ee12e6
nguyễn mạnh <manharia123@gmail.com>
2024-02-29 00:02:52 +0700

    ôn tâp

---
+++ khuyến_mại_bia;/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace khuyến_mại_bia_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhập số chai bia ban đầu anh ấy mua: ");
+            int n = int.Parse(Console.ReadLine());
+            sobia(n);
+            Console.ReadLine();
+        }
+
+        static void sobia(int n)
+        {
+            int solgthuong = n % 10;
+            int tongChaiBia = n + ((n / 10) * 3);
+            Console.WriteLine("Tất cả số bia là: " + tongChaiBia);
+        }
+    }
+}
